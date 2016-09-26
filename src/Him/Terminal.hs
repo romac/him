1113bf31bfd57444895c33d167edb9b9f9242ece
Romain Ruetschi <romain.ruetschi@gmail.com>
2016-09-26 20:40:45 +0200

    Enumerate all TerminalModes generically instead of hardcoding them.

--- src/Him/Terminal.hs
+++ src/Him/Terminal.hs
@@ -1,53 +1,71 @@
+
+{-# LANGUAGE StandaloneDeriving #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE DeriveAnyClass #-}
+
 module Him.Terminal
-  ( withRawInput ) where
+  ( withRawInput
+  ) where
 
 import           Control.Exception (finally, catch, Exception, IOException)
 import           Data.Typeable (Typeable)
 import           System.Posix.IO (stdInput)
-import           System.Posix.Terminal (TerminalMode(..), TerminalAttributes(..), TerminalState(..), withoutMode, getTerminalAttributes, setTerminalAttributes)
+import           System.Posix.Terminal
+                   ( TerminalMode(..), TerminalAttributes(..), TerminalState(..)
+                   , withoutMode, getTerminalAttributes, setTerminalAttributes
+                   )
+
+import           GHC.Generics
+import           Generics.Deriving.Enum
+
+deriving instance Generic TerminalMode
+deriving instance GEnum TerminalMode
 
 allTermModes :: [TerminalMode]
-allTermModes =
-  [ InterruptOnBreak
-  , MapCRtoLF
-  , IgnoreBreak
-  , IgnoreCR
-  , IgnoreParityErrors
-  , MapLFtoCR
-  , CheckParity
-  , StripHighBit
-  , StartStopInput
-  , StartStopOutput
-  , MarkParityErrors
-  , ProcessOutput
-  , LocalMode
-  , ReadEnable
-  , TwoStopBits
-  , HangupOnClose
-  , EnableParity
-  , OddParity
-  , EnableEcho
-  , EchoErase
-  , EchoKill
-  , EchoLF
-  , ProcessInput
-  , ExtendedFunctions
-  , KeyboardInterrupts
-  , NoFlushOnInterrupt
-  , BackgroundWriteInterrupt
-  ]
+allTermModes = genum
+
+-- allTermModes =
+  -- [ InterruptOnBreak
+  -- , MapCRtoLF
+  -- , IgnoreBreak
+  -- , IgnoreCR
+  -- , IgnoreParityErrors
+  -- , MapLFtoCR
+  -- , CheckParity
+  -- , StripHighBit
+  -- , StartStopInput
+  -- , StartStopOutput
+  -- , MarkParityErrors
+  -- , ProcessOutput
+  -- , LocalMode
+  -- , ReadEnable
+  -- , TwoStopBits
+  -- , HangupOnClose
+  -- , EnableParity
+  -- , OddParity
+  -- , EnableEcho
+  -- , EchoErase
+  -- , EchoKill
+  -- , EchoLF
+  -- , ProcessInput
+  -- , ExtendedFunctions
+  -- , KeyboardInterrupts
+  -- , NoFlushOnInterrupt
+  -- , BackgroundWriteInterrupt
+  -- ]
 
 rawTermSettings :: TerminalAttributes -> TerminalAttributes
 rawTermSettings = foldr (.) id funs
   where funs = flip withoutMode <$> allTermModes
 
-setTermAttrsNow settings = setTerminalAttributes stdInput settings Immediately
+setTerminalAttributesNow :: TerminalAttributes -> IO ()
+setTerminalAttributesNow attrs = setTerminalAttributes stdInput attrs Immediately
 
 withRawInput :: IO a -> IO a
 withRawInput application = do
   oldTermSettings <- getTerminalAttributes stdInput
   let newTermSettings = rawTermSettings oldTermSettings
-  setTermAttrsNow newTermSettings
+  setTerminalAttributesNow newTermSettings
   application
-    `finally` setTermAttrsNow oldTermSettings
+    `finally` setTerminalAttributesNow oldTermSettings
 
